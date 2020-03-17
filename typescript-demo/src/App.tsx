6e42655665f839dfa1d7fe11324f7e25893c341f
hejiangle <jlhe@thoughtworks.com>
2020-03-17 18:41:53 +0800

    Add a simaple typescript demo.

---
+++ typescript-demo/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Jiangle is ready to learn React.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
